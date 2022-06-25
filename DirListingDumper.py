8d445125d2db40000e9077a621bdacf72535674d
peterpan <peterpan@clochette.home>
2022-06-25 03:22:59 +0200

    DirListingDumper 1.0

--- DirListingDumper.py
+++ DirListingDumper.py
@@ -1,24 +1,27 @@
-from ast import arg
-from email.mime import base
 import requests
 from bs4 import BeautifulSoup
-from threading import Thread
 import pathlib
 import argparse
-
-url = "http://104.210.219.69"
-
-PATH_TO_DOWNLOAD = "/tmp/"
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 banned_words = ["?C=N;O=D", "?C=M;O=A", "?C=S;O=A", "?C=D;O=A"]
-
 to_download = []
 to_scrap = []
+threads_list = []
+download_failed = []
 
-r = requests.get(url)
-
-soup = BeautifulSoup(r.text, "lxml")
-
+class bcolors:
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    GREYBG = "\33[100m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    CYELLOW = "\33[33m"
+    REDBG = "\33[101m"
+    
 
 def crawl_directory_listing(base_url, endpoint=""):
 
@@ -42,6 +45,39 @@ def crawl_directory_listing(base_url, endpoint=""):
             # Thread(target=crawl_directory_listing, args=(url_formated,)).start()
             crawl_directory_listing(url_formated)
 
+def download_files(url):
+    url_without_http = url.replace("http://", "")
+    endpoints = url_without_http.split("/")
+    directory_to_create_on_system = "/".join(endpoints[1:-1])
+    file_name = endpoints[::-1][0]
+    pathlib.Path(f"{system_path}/{host_directory}/{directory_to_create_on_system}").mkdir(parents=True, exist_ok=True)
+    i = 0
+    while i <= 4:
+        try:
+            response = requests.get(url)
+        except Exception as e:
+            i+=1
+            print(f"{bcolors.OKBLUE}[❌]{bcolors.ENDC} {bcolors.CYELLOW}{system_path}/{host_directory}/{directory_to_create_on_system}/{bcolors.ENDC}{bcolors.BOLD}{bcolors.OKBLUE}{file_name}{bcolors.ENDC}".replace("//", "/"))
+            continue
+
+        if response.status_code == 200:
+            with open(f"{system_path}/{host_directory}/{directory_to_create_on_system}/{file_name}", "wb") as f:
+                f.write(response.content)
+            len_content = len(response.content)
+            if len_content < 100000:
+                len_str = f"{len_content/1000} KB"
+            else:
+                len_str = f"{len_content/1000000} MB"
+            print(f"{bcolors.OKBLUE}[📥]{bcolors.ENDC} {bcolors.CYELLOW}{system_path}/{host_directory}/{directory_to_create_on_system}/{bcolors.ENDC}{bcolors.BOLD}{bcolors.OKBLUE}{file_name}{bcolors.ENDC} - {bcolors.OKGREEN}{len_str}{bcolors.ENDC}".replace("//", "/"))
+            return 0
+        else:
+            i+=1
+            print(f"{bcolors.OKBLUE}[❌]{bcolors.ENDC} {bcolors.CYELLOW}{system_path}/{host_directory}/{directory_to_create_on_system}/{bcolors.ENDC}{bcolors.BOLD}{bcolors.OKBLUE}{file_name}{bcolors.ENDC}".replace("//", "/"))
+
+            continue
+    download_failed.append("    - " + url)
+    return 1
+
 
 def format_url(url_to_format):
     url_split = url_to_format.split("http://")
@@ -50,24 +86,42 @@ def format_url(url_to_format):
     return url_formated
 
 
-crawl_directory_listing(url)
+if __name__ == "__main__":
+    without_threads_start = time.time()
 
-host_directory = url.replace("http://", "")
-
-host_directory = host_directory.strip("/")
-PATH_TO_DOWNLOAD = PATH_TO_DOWNLOAD.rstrip("/")
-
-for url in to_download:
-    url_without_http = url.replace("http://", "")
-    endpoints = url_without_http.split("/")
-    directory_to_create_on_system = "/".join(endpoints[1:-1])
-    file_name = endpoints[::-1][0]
-    pathlib.Path(f"{PATH_TO_DOWNLOAD}/{host_directory}/{directory_to_create_on_system}").mkdir(parents=True, exist_ok=True)
-    response = requests.get(url)
-    print(len(response.content))
-    with open(f"{PATH_TO_DOWNLOAD}/{host_directory}/{directory_to_create_on_system}/{file_name}", "wb") as f:
-        f.write(response.content)
-    print(
-        f"{PATH_TO_DOWNLOAD}/{host_directory}/{directory_to_create_on_system}",
-        file_name,
+    parser = argparse.ArgumentParser(
+        description="Dump directory listing : python3 DirListingDumper.py http://dirlisting.com /tmp/"
     )
+    parser.add_argument(
+        "website",
+        help='URL where the directory listing is located.',
+    )
+    parser.add_argument("system_path", help="System file where the dump is saved.")
+
+
+
+    args = parser.parse_args()
+
+    website = args.website
+    system_path = args.system_path
+
+    crawl_directory_listing(website)
+
+    host_directory = website.replace("http://", "")
+
+    host_directory = host_directory.strip("/")
+    system_path = system_path.rstrip("/")
+
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        futures = [executor.submit(download_files, url) for url in to_download]
+        for future in futures:
+            result = future.result()
+
+    if len(download_failed) == 0:
+        print(f"\n{bcolors.REDBG}Failed download(s): {bcolors.ENDC}\n" + \
+            "   - None\n")
+    else:
+        print(f"\n{bcolors.REDBG}Failed download(s): {bcolors.ENDC}\n" + \
+          '\n'.join(download_failed) + "\n")
+    
+    print(f"{bcolors.OKBLUE}[⌛]{bcolors.ENDC} Execution time: {bcolors.GREYBG}{time.time() - without_threads_start}{bcolors.ENDC} secs.")
